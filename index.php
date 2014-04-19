8c585c16cebeca31d18ae69a356f0eaee4864d1d
Igor Kozlov <varrekminosh@gmail.com>
2014-04-19 16:17:22 +0300

    Changed program logic to view unique character from string

--- index.php
+++ index.php
@@ -14,7 +14,5 @@ if (!$string) {
     echo "Nothing to output\n";
 } else {
     echo "String you input:\n $string \n";
-    foreach (count_chars($string, 1) as $i => $val) {
-        echo "There were $val instance(s) of \"" , chr($i) , "\" in the string.\n";
-    }
+    echo "Unique character: ".count_chars($string, 3)."\n";
 }
