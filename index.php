238a5c1fe1be7253a2001a5daf3c9a1b7ed6e08f
Igor Kozlov <varrekminosh@gmail.com>
2014-04-20 16:09:45 +0300

    Not allowed empty input: disabled (\n), read strem till not empty string

--- index.php
+++ index.php
@@ -2,21 +2,20 @@
 function read_stdin()
 {
     $inputHandler = fopen("php://stdin", "r");
+    $input='';
+    while (!$input){
     $input = fgets($inputHandler, 128);
     $input=str_replace("\n","",$input);
+    }
     fclose($inputHandler);
     return $input;
 }
 
-echo "Please input string:\n";
+echo "Please input not empty string:\n";
 $string = read_stdin();
-if (!$string) {
-    echo "Nothing to output\n";
-} else {
     echo "String you input:\n $string \n";
     echo "Total chars:\n".strlen($string)."\n";
     echo "Unique character: ".count_chars($string, 3)."\n";
     foreach (count_chars($string, 1) as $i => $val) {
         echo "There were $val instance(s) of \"" , chr($i) , "\" in the string.\n";
     }
-}
