f24d3d569091ba6ebde464621a971c4b563dced4
Igor Kozlov <varrekminosh@gmail.com>
2014-04-22 22:06:17 +0300

    Change same string for merge conflict

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $string = read_stdin();
 if (!$string) {
     echo "Nothing to output\n";
 } else {
-    echo "String you input:\n $string \n";
+    echo "String you input are next:\n $string \n";
     echo "Total chars:\n".strlen($string)."\n";
     echo "Unique character: ".count_chars($string, 3)."\n";
     foreach (count_chars($string, 1) as $i => $val) {
