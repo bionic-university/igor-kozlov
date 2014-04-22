29d3fea1b76f2b0221a471ce67445925301e65e5
Igor Kozlov <varrekminosh@gmail.com>
2014-04-22 22:29:00 +0300

    Fix program logic to work with Russian strings

--- index.php
+++ index.php
@@ -10,16 +10,31 @@ function read_stdin()
     fclose($inputHandler);
     return $input;
 }
-
+//mb_strele]n & mb_subctr - we can add encoding
+function mb_count_chars($input) {
+    $l = mb_strlen($input, 'UTF-8');
+    $unique = array();
+    for($i = 0; $i < $l; $i++) {
+        $char = mb_substr($input, $i, 1, 'UTF-8');
+        if(!array_key_exists($char, $unique))
+            $unique[$char] = 0;
+        $unique[$char]++;
+    }
+    return $unique;
+}
 echo "Please input not empty string:\n";
 $string = read_stdin();
 if (!$string) {
     echo "Nothing to output\n";
 } else {
     echo "String you input are next:\n $string \n";
-    echo "Total chars:\n" . strlen($string) . "\n";
-    echo "Unique character: " . count_chars($string, 3) . "\n";
-    foreach (count_chars($string, 1) as $i => $val) {
-        echo "There were $val instance(s) of \"", chr($i), "\" in the string.\n";
+    echo "Total chars:\n" . mb_strlen($string,'UTF-8') . "\n";
+    $unique="";
+    foreach (mb_count_chars($string) as $i => $val) {
+        $unique.=$i.' ';
+    }
+    echo "Unique character: " .$unique."\n";
+    foreach (mb_count_chars($string) as $i => $val) {
+        echo "There were $val instance(s) of \"", $i, "\" in the string.\n";
     }
 }
