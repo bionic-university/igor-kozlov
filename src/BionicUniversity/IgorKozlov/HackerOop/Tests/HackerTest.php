08357e14b015e053b457bb92176678e13de6310e
Igor Kozlov <varrekminosh@gmail.com>
2014-05-28 11:41:49 +0300

    Test and chekstyle fix

--- src/BionicUniversity/IgorKozlov/HackerOop/Tests/HackerTest.php
+++ src/BionicUniversity/IgorKozlov/HackerOop/Tests/HackerTest.php
@@ -4,8 +4,15 @@ namespace BionicUniversity\IgorKozlov\HackerOop\Tests;
 
 use BionicUniversity\IgorKozlov\HackerOop\Code\Hacker;
 
+/**
+ * Class HackerTest
+ * @package BionicUniversity\IgorKozlov\HackerOop\Tests
+ */
 class HackerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Test setting fullname
+     */
     public function testSetFullname()
     {
         $hacker = new Hacker();
@@ -14,12 +21,18 @@ class HackerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($fullname, $hacker->getFullname());
     }
 
+    /**
+     * Test getting fullname
+     */
     public function testGetFullname()
     {
         $hacker = new Hacker('Igor Kozlov');
         $this->assertEquals('Igor Kozlov', $hacker->getFullname());
     }
 
+    /**
+     * Test setting nickname
+     */
     public function testSetNickname()
     {
         $hacker = new Hacker();
@@ -28,6 +41,9 @@ class HackerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($nickname, $hacker->getNickname());
     }
 
+    /**
+     * Test setting toolsToScan
+     */
     public function testSetToolsToScan()
     {
         $hacker = new Hacker();
@@ -38,20 +54,49 @@ class HackerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($toolsToScan, $hacker->getToolsToScan());
     }
 
-    public function testSetProficiency() {
+    /**
+     * Test setting proficiency
+     */
+    public function testSetProficiency()
+    {
         $hacker = new Hacker();
-        $proficiency=rand(0,2);
+        $proficiency = rand(0, 2);
         $hacker->setProficiency($proficiency);
         $this->assertEquals($proficiency, $hacker->getProficiency());
     }
 
+    /**
+     * Test setting toolsToSelfProtect
+     */
     public function testSetToolsToSelfProtect()
     {
         $hacker = new Hacker();
         $toolsToSelfProtect = array(
-            'arachni', 'ARMITAGE',"NODE32"
+            'arachni', 'ARMITAGE', "NODE32"
         );
         $hacker->setToolsToSelfProtect($toolsToSelfProtect);
         $this->assertEquals($toolsToSelfProtect, $hacker->getToolsToSelfProtect());
+    }
+
+    /**
+     * Test hacking with wrong classname
+     */
+    public function testHackWrongName()
+    {
+        $hacker = new Hacker();
+        $string = "AFGD";
+        $expectedResult = "Wrong class name!!!" . PHP_EOL;
+        $this->assertEquals($expectedResult, $hacker->hack($string));
+    }
+
+    /**
+     * Test hacking with wrong classname
+     */
+    public function testHackCantHack()
+    {
+        $hacker = new Hacker();
+        $string = "Emails";
+        $expectedResult = "You cant hack this!!!" . PHP_EOL;
+        $this->assertEquals($expectedResult, $hacker->hack($string));
     }
 }
