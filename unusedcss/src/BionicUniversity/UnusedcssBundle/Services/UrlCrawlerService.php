5676dbe06e178d75e9346511413f34b5942fe0ea
Igor Kozlov <varrekminosh@gmail.com>
2014-06-19 13:15:54 +0300

    Code refactoring Updated tests Added TODO-s based on test result

--- unusedcss/src/BionicUniversity/UnusedcssBundle/Services/UrlCrawlerService.php
+++ unusedcss/src/BionicUniversity/UnusedcssBundle/Services/UrlCrawlerService.php
@@ -3,6 +3,7 @@ namespace BionicUniversity\UnusedcssBundle\Services;
 
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpKernel\Client;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class UrlCrawlerService
 {
@@ -22,6 +23,53 @@ class UrlCrawlerService
     protected $domainLinks = array();
 
     /**
+     * @var ArrayCollection
+     */
+    protected $classes;
+
+    /**
+     * @var ArrayCollection
+     */
+    protected $ids;
+
+    /**
+     * @var ArrayCollection
+     */
+    protected $stylesheets;
+
+    /**
+     * @var ArrayCollection
+     */
+    protected $CSSclasses;
+
+    /**
+     * @var ArrayCollection
+     */
+    protected $CSSids;
+
+
+    public function __construct($site_url)
+    {
+        $this->site_url = $site_url;
+        $this->domain = parse_url($site_url, PHP_URL_HOST);
+        $this->domainLinks = array();
+        $this->classes = new ArrayCollection();
+        $this->ids = new ArrayCollection();
+        $this->stylesheets = new ArrayCollection();
+        $this->CSSclasses = new ArrayCollection();
+        $this->CSSids = new ArrayCollection();
+    }
+
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getClasses()
+    {
+        return $this->classes;
+    }
+
+    /**
      * @return array
      */
     public function getDomainLinks()
@@ -29,11 +77,36 @@ class UrlCrawlerService
         return $this->domainLinks;
     }
 
-    public function __construct($site_url)
+    /**
+     * @return ArrayCollection
+     */
+    public function getIds()
     {
-        $this->site_url = $site_url;
-        $this->domain = parse_url($site_url, PHP_URL_HOST);
-        $this->domainLinks = array();
+        return $this->ids;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getStylesheets()
+    {
+        return $this->stylesheets;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getCSSclasses()
+    {
+        return $this->CSSclasses;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getCSSids()
+    {
+        return $this->CSSids;
     }
 
     public function execute()
@@ -65,36 +138,50 @@ class UrlCrawlerService
             $this->domainLinks[] = $link;
         }
     }
-
-    function parseCSS($css)
+    //@TODO: find solution for double classes
+    private function parseCSSclasses($css)
     {
-        preg_match_all('/(?<=\\#)[_A-Za-z0-9\\-]+(?=[ ]*[{:])/', $css, $matches, PREG_SET_ORDER);
-        foreach ($matches as $val) {
-            var_dump($val);
-        }
         preg_match_all('/(?<=\\.)[_A-Za-z0-9\\-]+(?=[ ]*[{:])/', $css, $matches, PREG_SET_ORDER);
         foreach ($matches as $val) {
             var_dump($val);
+            $this->CSSclasses->add($val[0]);
         }
     }
 
-    public function getClassAndId($link)
+    private function parseCssIds($css)
     {
-        $html = file_get_contents($link);
-        $crawler = new Crawler();
-        $crawler->addHtmlContent($html);
+        preg_match_all('/(?<=\\#)[_A-Za-z0-9\\-]+(?=[ ]*[{:])/', $css, $matches, PREG_SET_ORDER);
+        foreach ($matches as $val) {
+            $this->CSSids->add($val[0]);
+        }
+    }
+
+    //@TODO: find solution for double classes
+    private function findClasses(Crawler $crawler)
+    {
         $crawler->filter('')->each(function ($node, $i) {
             if (!empty($node->attr('class'))) {
-                echo $node->attr('class') . PHP_EOL;
-            }
-            if (!empty($node->attr('id'))) {
-                echo $node->attr('id') . PHP_EOL;
+                $this->classes->add($node->attr('class'));
             }
         });
+    }
+
+    private function findIds(Crawler $crawler)
+    {
+        $crawler->filter('')->each(function ($node, $i) {
+            if (!empty($node->attr('id'))) {
+                $this->ids->add($node->attr('id'));
+            }
+        });
+    }
+
+    private function findStylesheet(Crawler $crawler)
+    {
         $crawler->filter('link[rel=stylesheet]')->each(function ($node, $i) {
             echo $node->attr('href') . PHP_EOL;
-            $css = file_get_contents('http://' . $this->domain . $node->attr('href'));
-            var_dump($this->parseCSS($css));
+            if (!empty($node->attr('href'))) {
+                $this->stylesheets->add($node->attr('href'));
+            }
         });
     }
 }
--- unusedcss/src/BionicUniversity/UnusedcssBundle/Tests/Services/UrlCrawlerServiceTest.php
+++ unusedcss/src/BionicUniversity/UnusedcssBundle/Tests/Services/UrlCrawlerServiceTest.php
@@ -2,9 +2,66 @@
 namespace BionicUniversity\UnusedcssBundle\Tests\Services;
 
 use BionicUniversity\UnusedcssBundle\Services\UrlCrawlerService;
+use Symfony\Component\DomCrawler\Crawler;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class UrlCrawlerServiceTest extends \PHPUnit_Framework_TestCase
 {
+
+    protected $html;
+    protected $css;
+
+    protected function setUp()
+    {
+        $this->html = '
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <link rel="stylesheet" href="/baseCss.css">
+            </head>
+            <body>
+                <p class="message temp" id="first">Hello World!</p>
+                <p id="second">Hello Crawler!</p>
+                <div class="trend"><a href="#" class="link" id="link"></a></div>
+            </body>
+        </html>';
+        $this->css = '
+                    .maincontent-bot {
+                      background: url(images/parallax-bot.png) no-repeat center 0 transparent;
+                      height: 50px;
+                      position:relative;
+                      z-index:1;
+                    }
+                    #left-para-mask {
+                        background: url("images/left-para-mask.png") repeat scroll 0 0 transparent;
+                        height: 400px;
+                        float: left;
+                        position: relative;
+                        margin-top: -400px;
+                        width: 130px;
+                        z-index: 2;
+                    }
+                    #right-para-mask {
+                        background: url("images/right-para-mask.png") repeat scroll 0 0 transparent;
+                        float: right;
+                        height: 400px;
+                        position: relative;
+                        margin-top: -400px;
+                        width: 130px;
+                        z-index: 2;
+                    }
+                    .block-center {
+                        border-top: 1px solid #F0F1F8;
+                        margin: 30px 0 -30px !important;
+                        padding: 25px 0 0;
+                        text-align: center;
+                    }
+                    .block-center a {
+                      color: #008ec9;
+                    }
+                    ';
+    }
+
     /*public function testGetLinkOnCurrentPage()
     {
         $crawl = new UrlCrawlerService('http://seo.imaginarium.in.ua');
@@ -20,11 +77,77 @@ class UrlCrawlerServiceTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(true, true);
     }
 */
-    public function testGetClass()
+    public function testFindClass()
     {
-        $crawl = new UrlCrawlerService('http://wizardry.ua/company/');
-        $crawl->getClassAndId('http://wizardry.ua/company/');
-        $this->assertEquals(true, true);
+        $crawler = new Crawler($this->html);
+        $expected = new ArrayCollection();
+        $expected->add('message temp');
+        $expected->add('trend');
+        $expected->add('link');
+        $crawl = new UrlCrawlerService('http://seo.imaginarium.in.ua');
+        $myReflection = new \ReflectionClass($crawl);
+        $reflectionMethod = $myReflection->getMethod("findClasses");
+        $reflectionMethod->setAccessible(true);
+        $reflectionMethod->invokeArgs($crawl, array($crawler));
+        $actual = $crawl->getClasses();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testFindIds()
+    {
+        $crawler = new Crawler($this->html);
+        $expected = new ArrayCollection();
+        $expected->add('first');
+        $expected->add('second');
+        $expected->add('link');
+        $crawl = new UrlCrawlerService('http://seo.imaginarium.in.ua');
+        $myReflection = new \ReflectionClass($crawl);
+        $reflectionMethod = $myReflection->getMethod("findIds");
+        $reflectionMethod->setAccessible(true);
+        $reflectionMethod->invokeArgs($crawl, array($crawler));
+        $actual = $crawl->getIds();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testFindStylesheet()
+    {
+        $crawler = new Crawler($this->html);
+        $expected = new ArrayCollection();
+        $expected->add('/baseCss.css');
+        $crawl = new UrlCrawlerService('http://seo.imaginarium.in.ua');
+        $myReflection = new \ReflectionClass($crawl);
+        $reflectionMethod = $myReflection->getMethod("findStylesheet");
+        $reflectionMethod->setAccessible(true);
+        $reflectionMethod->invokeArgs($crawl, array($crawler));
+        $actual = $crawl->getStylesheets();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testParseCssIds()
+    {
+        $crawl = new UrlCrawlerService('http://seo.imaginarium.in.ua');
+        $expected = new ArrayCollection();
+        $expected->add('left-para-mask');
+        $expected->add('right-para-mask');
+        $myReflection = new \ReflectionClass($crawl);
+        $reflectionMethod = $myReflection->getMethod("parseCssIds");
+        $reflectionMethod->setAccessible(true);
+        $reflectionMethod->invokeArgs($crawl, array($this->css));
+        $actual = $crawl->getCSSids();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testParseCssClasses()
+    {
+        $crawl = new UrlCrawlerService('http://seo.imaginarium.in.ua');
+        $expected = new ArrayCollection();
+        $expected->add('maincontent-bot');
+        $expected->add('block-center');
+        $myReflection = new \ReflectionClass($crawl);
+        $reflectionMethod = $myReflection->getMethod("parseCSSclasses");
+        $reflectionMethod->setAccessible(true);
+        $reflectionMethod->invokeArgs($crawl, array($this->css));
+        $actual = $crawl->getCSSclasses();
+        $this->assertEquals($expected, $actual);
     }
 }
- 
