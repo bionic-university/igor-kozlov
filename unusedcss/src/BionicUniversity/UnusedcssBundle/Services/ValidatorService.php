d5ad901b3b541b8724e2dddb3c7cac4f22cd268a
Igor Kozlov <varrekminosh@gmail.com>
2014-06-23 11:30:00 +0300

    Validation service for w3c html validation. Validation of all founded URLs (simply returts true or false)

---
+++ unusedcss/src/BionicUniversity/UnusedcssBundle/Services/ValidatorService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: varrek
+ * Date: 23.06.14
+ * Time: 9:54
+ */
+
+namespace BionicUniversity\UnusedcssBundle\Services;
+
+
+class ValidatorService {
+
+} 
---
+++ unusedcss/src/BionicUniversity/UnusedcssBundle/Tests/Services/ValidatorServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: varrek
+ * Date: 23.06.14
+ * Time: 10:26
+ */
+
+namespace BionicUniversity\UnusedcssBundle\Tests\Services;
+
+
+class ValidatorServiceTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 
